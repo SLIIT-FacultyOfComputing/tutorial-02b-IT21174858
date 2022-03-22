a983165986aa2cf4c8b096ca08e023e546288340
it21174858 <99777444+IT21174858@users.noreply.github.com>
2022-03-22 08:58:40 +0000

    change

--- tute03.cpp
+++ tute03.cpp
@@ -5,20 +5,20 @@ Convert the C program given below which calculates the Factorial of a number tha
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
 #include <iostream>
-using namespace std;
 int main()
 {
     int no;
     long fac;
 
-    cout<<"Enter a Number :";
-    cin>>no;
+    std::cout<<"Enter a Number :";
+    std::cin>>no;
 
     fac = 1;
-    for (int r=no; r >= 1; r--) {
+    for (int r=no; r >= 1; r--)
+	 {
         fac = fac * r;
     }
 
-    cout<<"Factorial of is:"<<fac;  
+    std::cout<<"Factorial of is:" << fac;
     return 0;
 }
