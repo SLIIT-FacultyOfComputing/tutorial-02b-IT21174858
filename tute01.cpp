52a1776307511d98c78a88de556b831500d864bf
it21174858 <99777444+IT21174858@users.noreply.github.com>
2022-03-22 07:05:34 +0000

    finished

--- tute01.cpp
+++ tute01.cpp
@@ -16,15 +16,13 @@ You already know that printf() in C is std::cout in C++ e.g.
 printf("Hello World") --> std::cout << "Hello World";
 2.54cm = 1 inch
 */
-
-
-#include <stdio.h>
-int main() 
+#include<iostream>
+using namespace std;
+int main()
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
-    return 0;
-}  
+  float cm,inches;
+  cout<<"Enter a length in cm:";
+  cin>>cm;
+  inches=cm/2.54;
+  cout<<"Length in inches is"<<inches;
+}
